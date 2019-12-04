8f94eccd166a8f8fd08e005b462cf94e9dd282e1
elCrixtof <45042863+elCrixtof@users.noreply.github.com>
2019-12-04 02:39:30 -0600

    Controlador API

---
+++ PrestaLabDICIS/PrestaLabDICIS.Web/Controllers/API/ArticulosController.cs
@@ -0,0 +1,22 @@
+﻿namespace PrestaLabDICIS.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using PrestaLabDICIS.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class ArticulosController : Controller
+    {
+        private readonly IArticuloRepository articuloRepository;
+
+        public ArticulosController(IArticuloRepository articuloRepository)
+        {
+            this.articuloRepository = articuloRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetArticulos()
+        {
+            return Ok(this.articuloRepository.GetAll());
+        }
+    }
+}
